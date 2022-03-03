33bd9a96f1f1734b5e27d6b34879be1f7e17aa5a
cy-kim <kimseongyeun@gmail.com>
2022-03-02 23:16:30 -0500

    fist

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+  }
+  socket.emit("yourID", socket.id);
+  io.sockets.emit("allUsers", users);
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("hey", {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+
+  socket.on("acceptCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+if (process.env.PROD){
+    app.use(express.static(path.join(__dirname, './client/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname, './client/build/index.html'))
+    })
+}
+
+server.listen(process.env.PORT || 7000, () =>
+  console.log("server is running on port 8000")
+);
